6451c8cd94976dd6a8177c5762fb10036ccc4939
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-12 10:54:12 +0100

    Interface / classes

--- src/app/view/etudiants/etudiants.ts
+++ src/app/view/etudiants/etudiants.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Etudiant{
+  nom: string;
+  prenom: string;
+  classe: string;
+  dateNaissance?: string;
+  sexe: string;
+  filiere: string;
+  niveau: string;
+}
+
 @Component({
   selector: 'app-etudiants',
   imports: [],
@@ -16,4 +26,11 @@ export class Etudiants {
     private matricule: string = "2024-ISI-001";
     is_isi4: boolean = true;
     public students: string[] = ['KOUANG', 'KOUBISSACK', 'BILOGUE', 'DONGMO', 'MOUGANG'];
+    list_students: Etudiant[] = [
+      { nom: 'KOUANG', prenom: 'Jackie', classe:'ISI 4 FR', sexe: 'F', filiere: 'ISI', niveau: '4' },
+      { nom: 'KOUBISSACK', prenom: 'Lucie', classe:'ISI 4 FR', dateNaissance: '12-09-2004', sexe: 'F', filiere: 'ISI', niveau: '4' },
+      { nom: 'BILOGUE', prenom: 'Jean', classe:'ISI 4 FR', dateNaissance: '12-09-2005', sexe: 'M', filiere: 'ISI', niveau: '4' },
+      { nom: 'DONGMO', prenom: 'Jean', classe:'ISI 4 FR', dateNaissance: '12-09-2006', sexe: 'M', filiere: 'ISI', niveau: '4' },
+      { nom: 'MOUGANG', prenom: 'Jean', classe:'ISI 4 FR', dateNaissance: '24-02-2004', sexe: 'M', filiere: 'ISI', niveau: '4' }
+    ];
 }
