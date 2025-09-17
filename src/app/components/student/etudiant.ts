e4261d82c4f1170abe8bdbf6eccefc16f6bb1b45
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-17 16:00:11 +0100

    Definition de l'interface etudiant

---
+++ src/app/components/student/etudiant.ts
@@ -0,0 +1,7 @@
+class Etudiant {
+  matricule!:string
+  nom!:string
+  prenom?:string
+  dateNaissance!:string
+  
+}
---
+++ src/app/components/student/student.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.scss'
+})
+
+
+export class Student {
+  @Input() etudiant!:Etudiant
+}
