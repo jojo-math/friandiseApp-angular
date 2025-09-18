cd41507848d100473680ded2f5a72e4d6824497b
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-18 16:18:53 +0100

    Custom header #2, Custom footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Etudiants  } from './view/etudiants/etudiants';
 import { Navbar } from "./view/navbar/navbar";
+import { Footer } from "./view/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
