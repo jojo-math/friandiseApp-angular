eb3e3609d282307aba9cd6a3eb87fbbaee954f91
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-18 14:25:52 +0100

    Deplacement du dossier about vers la vue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Etudiants } from './view/etudiants/etudiants';
 import { Navbar } from './view/navbar/navbar';
 import { Footer } from './view/footer/footer';
-import { About } from './about/about';
+import { About } from './view/about/about';
 import { Student } from './components/student/student';
 import { DetailsStudent } from './view/details-student/details-student';
 
